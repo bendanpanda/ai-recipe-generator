07f71ef06ddbb8ea8c3e0132e1baded0215c90b7
Ivanka <162492337+bendanpanda@users.noreply.github.com>
2024-07-06 18:22:11 +0100

    Added the AI API integration
    Added axios + AI API integration.
    Played around with the context and prompt.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-function generateRecipe(event) {
-  event.preventDefault();
+function displayRecipe(response) {
+  console.log("Recipe generated!");
 
   new Typewriter("#recipe", {
-    strings: "Generating a recipe",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "a4a3o83044e30bf2e965a940tb084f55";
+  let context =
+    "You are an experienced AI chef with extensive knowledge of all cuisines. Your goal is to create a recipe in basic HTML and separate each line with a <br />. The recipe has to have a clear title, brief description of the dish, total cooking time, a list of ingredients with measurements, step-by-step cooking instructions, tips for best results. Make sure to follow the user instructions. At the end sign the recipe with 'SheCodes AI' inside a <strong> element.";
+  let prompt = `User instructions: Generate a recipe from ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe..");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
