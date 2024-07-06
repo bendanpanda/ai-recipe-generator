cbbb0828b98d95d034dd671b78afc2ade8c876d2
Ivanka <162492337+bendanpanda@users.noreply.github.com>
2024-07-06 18:33:07 +0100

    Changing some wording and changing colour of the <strong>

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function generateRecipe(event) {
 
   let apiKey = "a4a3o83044e30bf2e965a940tb084f55";
   let context =
-    "You are an experienced AI chef with extensive knowledge of all cuisines. Your goal is to create a recipe in basic HTML and separate each line with a <br />. The recipe has to have a clear title, brief description of the dish, total cooking time, a list of ingredients with measurements, step-by-step cooking instructions, tips for best results. Make sure to follow the user instructions. At the end sign the recipe with 'SheCodes AI' inside a <strong> element.";
+    "You are an experienced AI chef with extensive knowledge of all cuisines. Your goal is to create a recipe in basic HTML and separate each line with a <br />. The recipe has to have a clear title inside an <h1>, brief description of the dish, total cooking time, a list of ingredients with measurements, step-by-step cooking instructions, tips for best results. Make sure to follow the user instructions. At the end sign the recipe with 'SheCodes AI' inside a <strong> element.";
   let prompt = `User instructions: Generate a recipe from ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
